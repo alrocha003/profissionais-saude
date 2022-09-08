cddf398e2f9fcc89b0911244395fc7a479b25fac
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-08 20:38:37 -0300

    :white_check_mark: feat: update index - remove class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
-import prompt from 'prompt-sync';
+import prompt from "prompt-sync";
+
 
 console.debug("\nBem vindo ao projeto - 🅿🆁🅾🅵🅸🆂🅸🅾🅽🅰🅸🆂 🅳🅰 🆂🅰ú🅳🅴\n");
 
-const input = prompt();
+let input = prompt();
+let option: number = 0;
 
-console.debug("------------Funcionalidades------------\n" +
-    " 1 - Cadastrar Paciente\n" +
-    " 2 - Remover Paciente\n" +
-    " 3 - Listar Pacientes");
+while (option != 9) {
+  console.log(
+    "------------Funcionalidades------------\n" +
+      " 1 - Cadastrar Paciente\n" +
+      " 2 - Remover Paciente\n" +
+      " 3 - Listar Pacientes"
+  );
 
-let option: Number = parseInt(input("\nInforme a opção desejada: "));
+  let option: Number = parseInt(input("\nInforme a opção desejada: "));
+
+  switch (option) {
+    case 1:
+      console.log(input("\nInsira o nome: "));
+      break;
+    case 2:
+      console.log("digite o nome do usuário");
+      break;
+    case 3:
+      console.log("lista dos pacientes");
+      break;
+    default:
+      break;
+  }
+}
 
 console.debug(option);
