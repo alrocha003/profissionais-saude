9a99665890d53b7ff4410a55890d426c7c372b9e
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-10 12:11:32 -0300

    :recycle: refactor: case 1 and case 2

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,27 @@ while (option != 4) {
   console.log('|4. Sair                        |');
   console.log('+================================+')
 
-  option = +input("Escolha uma funcionalidade")
+  option = +input("Escolha uma funcionalidade : --> ")
 
   switch (option) {
     case 1:
-      console.log("Insira o nome do Paciente: ");
+      const nomeEsobrenomePaciente = input('Digite o Nome e Sobrenome do Paciente : ')
+      const emailPaciente = input('Digite o Email: ')
+      const celularPaciente = input('Digite o celular: ')
+      const dataNascimentoPaciente = input('Digite a Data de Nascimento: ')
+      const pesoPaciente = input('Digite o Peso: ')
+      const alturaPaciente = input('Digite a Altura: ')
       break;
     case 2:
-      console.log("Insira o nome do Médico");
+      const nomeEsobrenomeMedico = input('Digite o Nome e Sobrenome do Médico : ')
+      const emailMedico = input('Digite o Email: ')
+      const celularMedico = input('Digite o celular: ')
+      const dataNascimentoMedico = input('Digite a Data de Nascimento: ')
+      const crm = input('Digite o CRM: ')
+      const especialidade = input('Digite a Especialidade: ')
       break;
     case 3:
-      console.log("Lista Pacientes/Médicos");
+      console.table('Lista do Paciente e Médico');
       break;
     case 4:
       console.log("Sair");
