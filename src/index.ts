edf9a8414bd194c0018f8b9c49ae126b31727e59
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-11 18:13:47 -0300

    :recycle: refactor case 1,2 and 3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import prompt from "prompt-sync";
+import Doctor from "./Doctor";
+import Patient from "./Patient";
 
 
 console.debug("\nBem vindo ao projeto - 🅿🆁🅾🅵🅸🆂🅸🅾🅽🅰🅸🆂 🅳🅰 🆂🅰ú🅳🅴\n");
 
 let input = prompt();
 let option: number = 0;
+let listaUsuarios: Array<IUser> = [];
 
 while (option != 4) {
   console.log('+========= Lista Funcionalidades =========+');
@@ -15,26 +18,43 @@ while (option != 4) {
   console.log('+================================+')
 
   option = +input("Escolha uma funcionalidade : --> ")
-
+  
   switch (option) {
     case 1:
-      const nomeEsobrenomePaciente = input('Digite o Nome e Sobrenome do Paciente : ')
-      const emailPaciente = input('Digite o Email: ')
-      const celularPaciente = input('Digite o celular: ')
-      const dataNascimentoPaciente = input('Digite a Data de Nascimento: ')
-      const pesoPaciente = input('Digite o Peso: ')
-      const alturaPaciente = input('Digite a Altura: ')
+      let nomeEsobrenomePaciente = input('Digite o Nome e Sobrenome do Paciente : ')
+      let emailPaciente = input('Digite o Email: ')
+      let celularPaciente = input('Digite o celular: ')
+      let dataNascimentoPaciente = input('Digite a Data de Nascimento: ')
+      let pesoPaciente = input('Digite o Peso: ')
+      let alturaPaciente = input('Digite a Altura: ')
+      let novoPaciente = new Patient(nomeEsobrenomePaciente,
+        emailPaciente,
+        celularPaciente,
+        dataNascimentoPaciente,
+        pesoPaciente,
+        alturaPaciente)
+        listaUsuarios.push(novoPaciente)
       break;
     case 2:
-      const nomeEsobrenomeMedico = input('Digite o Nome e Sobrenome do Médico : ')
-      const emailMedico = input('Digite o Email: ')
-      const celularMedico = input('Digite o celular: ')
-      const dataNascimentoMedico = input('Digite a Data de Nascimento: ')
-      const crm = input('Digite o CRM: ')
-      const especialidade = input('Digite a Especialidade: ')
+      let nomeEsobrenomeMedico = input('Digite o Nome e Sobrenome do Médico : ')
+      let emailMedico = input('Digite o Email: ')
+      let celularMedico = input('Digite o celular: ')
+      let dataNascimentoMedico = input('Digite a Data de Nascimento: ')
+      let crm = input('Digite o CRM: ')
+      let especialidade = input('Digite a Especialidade: ')
+      let novoMedico = new Doctor(nomeEsobrenomeMedico,
+        emailMedico,
+        celularMedico,
+        dataNascimentoMedico,
+        crm,
+        especialidade,)
+        listaUsuarios.push(novoMedico)
       break;
     case 3:
-      console.table('Lista do Paciente e Médico');
+      for(let i = 0; i< listaUsuarios.length; i++ ){
+       console.table(listaUsuarios[i].exibeDados())
+        
+      }
       break;
     case 4:
       console.log("Sair");
