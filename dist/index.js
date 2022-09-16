acce449a768a9bc0b7b154214c2b1a7794a1744e
alrocha003 <alrocha003@gmail.com>
2022-09-15 23:31:14 -0300

    fix: remove function

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const function_cadastro_1 = require("./commons/function-cadastro");
 console.debug("\nBem vindo ao projeto - 🅿🆁🅾🅵🅸🆂🅸🅾🅽🅰🅸🆂 🅳🅰 🆂🅰ú🅳🅴\n");
-const input = (0, prompt_sync_1.default)();
-console.debug("------------Funcionalidades------------\n" +
-    " 1 - Cadastrar Paciente\n" +
-    " 2 - Remover Paciente\n" +
-    " 3 - Listar Pacientes");
-let option = parseInt(input("\nInforme a opção desejada: "));
-console.debug(option);
+let input = (0, prompt_sync_1.default)();
+let option = 0;
+let cadastra = new function_cadastro_1.Cadastra();
+while (option != 5) {
+    console.log("+========= Lista Funcionalidades =========+");
+    console.log("|1. Cadastrar Paciente          |");
+    console.log("|2. Cadastrar Médico            |");
+    console.log("|3. Listar Paciente/Médico      |");
+    console.log("|4. Remover cadastro            |");
+    console.log("|5. Sair                        |");
+    console.log("+================================+");
+    option = +input("Escolha uma funcionalidade : --> ");
+    switch (option) {
+        case 1:
+            cadastra.cadastroPaciente();
+            break;
+        case 2:
+            cadastra.cadastroMedico();
+            break;
+        case 3:
+            cadastra.listar();
+            break;
+        case 4:
+            cadastra.remove();
+            break;
+        case 5:
+            console.log("Sair");
+            break;
+        default:
+            break;
+    }
+}
+console.log("Acabou!");
+//todo : criar o "remover" e "atualizar"
+// requisicao de exame (Pesquisa)
--- src/commons/function-cadastro.ts
+++ src/commons/function-cadastro.ts
@@ -2,30 +2,28 @@ import prompt from "prompt-sync";
 import Doctor from "../Doctor";
 import Patient from "../Patient";
 
-
-
 export class Cadastra {
   input = prompt();
   option: number = 0;
   listaUsuarios: Array<IUser> = [];
 
   cadastroPaciente() {
-     let nomeEsobrenomeMedico = this.input('Digite o Nome e Sobrenome do Médico : ');
-     let emailMedico = this.input('Digite o Email: ');
-     let celularMedico = this.input('Digite o celular: ');
-     let dataNascimentoMedico = this.input('Digite a Data de Nascimento: ');
-     let crm = this.input('Digite o CRM: ');
-     let especialidade = this.input('Digite a Especialidade: ');
-     let novoMedico = new Doctor(nomeEsobrenomeMedico,
-       emailMedico,
-       celularMedico,
-       dataNascimentoMedico,
-       crm,
-       especialidade);
-     this.listaUsuarios.push(novoMedico);
-   }
+    let nomeEsobrenomeMedico = this.input('Digite o Nome e Sobrenome do Médico : ');
+    let emailMedico = this.input('Digite o Email: ');
+    let celularMedico = this.input('Digite o celular: ');
+    let dataNascimentoMedico = this.input('Digite a Data de Nascimento: ');
+    let crm = this.input('Digite o CRM: ');
+    let especialidade = this.input('Digite a Especialidade: ');
+    let novoMedico = new Doctor(nomeEsobrenomeMedico,
+      emailMedico,
+      celularMedico,
+      dataNascimentoMedico,
+      crm,
+      especialidade);
+    this.listaUsuarios.push(novoMedico);
+  }
 
-   cadastroMedico() {
+  cadastroMedico() {
     let nomeEsobrenomePaciente = this.input('Digite o Nome e Sobrenome do Paciente : ');
     let emailPaciente = this.input('Digite o Email: ');
     let celularPaciente = this.input('Digite o celular: ');
@@ -43,21 +41,31 @@ export class Cadastra {
 
   listar = () => this.listaUsuarios.map(user => console.debug(user.exibeDados()));
 
-
+  remove() {
+    let nomeSobrenome = this.input('Digite o nome: \t');
+    let user = this.listaUsuarios.find(u => u.nomeSobrenome == nomeSobrenome);
+    if (user === undefined) {
+      console.log(`Esse ${nomeSobrenome} não existe no banco de dados!`);
+    } else {
+      this.listaUsuarios = this.listaUsuarios.filter(u => u.nomeSobrenome !== nomeSobrenome);
+      console.log('Esse cadastro foi removido');
+    }
+  }
 }
 
-export class Remover {
-  input = prompt();
-  option: number = 0;
-  listaUsuarios: Array<IUser> = [];
-  remove() {
-    let nomeSobrenome = this.input('Digite o nome')
-    let index = this.listaUsuarios.includes({nomeSobrenome})
-    let indexof = this.listaUsuarios.indexOf({nomeSobrenome})
-    if(!index){
-      console.log(`Esse ${nomeSobrenome} não existe no banco de dados!`)
-    } else if(index){
-      this.listaUsuarios.splice(indexof,1)
-      console.log('Esse cadastro foi removido')
-    }
-}}
+    // export class Remover {
+    //   input = prompt();
+    //   option: number = 0;
+    //   listaUsuarios: Array<IUser> = [];
+    //   remove() {
+    //     let nomeSobrenome = this.input('Digite o nome')
+    //     let index = this.listaUsuarios.includes({ nomeSobrenome })
+    //     let indexof = this.listaUsuarios.indexOf({ nomeSobrenome })
+    //     if (!index) {
+    //       console.log(`Esse ${nomeSobrenome} não existe no banco de dados!`)
+    //     } else if (index) {
+    //       this.listaUsuarios.splice(indexof, 1)
+    //       console.log('Esse cadastro foi removido')
+    //     }
+    //   }
+    // }
