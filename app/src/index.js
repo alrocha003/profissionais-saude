71cfa1e51a35036a94cf3ce5495fcd99a2878738
Gabriela Tavares <gabriela.ttavares@gmail.com>
2023-04-26 20:17:32 -0300

    feature: change post route

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,37 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
-const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const PORT = 3001;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded());
-
 app.get('/hello', (request, response) => {
-    response.render('hello');
+  response.render('hello');
 });
 
-app.get('/login', (request, response) => {
-    response.render('login/login.ejs');
+// const path = require('path');
+
+// app.route('/*')
+//   // eslint-disable-next-line prefer-arrow-callback, func-names
+//   .get(function (request, response) {
+//     // eslint-disable-next-line prefer-template
+//     response.sendFile(path.resolve(app.get() + '/login/login.ejs'));
+//   });
+
+app.get('/login', (req, res) => {
+  // eslint-disable-next-line no-path-concat, prefer-template
+  res.sendFile(__dirname + '/views/login/login.ejs');
+});
+
+app.post('/login', urlencodedParser, (request, response) => {
+  console.info('User:', request.body);
+  response.render('login/login.ejs');
 });
 
 app.listen(PORT, () => {
-    console.debug(`Executando na porta ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.debug(`Executando na porta ${PORT}`);
 });
